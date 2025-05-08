5554476855ac75cc22d12309b347b78a85159ac4
Enzzo <enzzoferreira69@gmail.com>
2025-05-08 19:35:19 -0300

    Adicionei o Trabalho do André

---
+++ trabalho do andré.py
@@ -0,0 +1,228 @@
+'''
+Implementar a opção 2 (procurar contato) da seguinte forma:
+Ficar pedindo para digitar um nome até digitar um nome que existe;
+mostrar então na tela TODOS os demais dados daquela pessoa, cujo
+nome foi digitado.
+
+Implementar a opção 3 (atualizar contato) da seguinte forma:
+Ficar mostrando um menu oferecendo as opções de atualizar aniversário, ou
+endereco, ou telefone, ou celular, ou email, ou finalizar as
+atualizações; ficar pedindo para digitar a opção até digitar uma
+opção válida; realizar a atulização solicitada; até ser escolhida a
+opção de finalizar as atualizações.
+
+Implementar a opção 4 (listar contato) da seguinte forma:
+Mostrar na tela os TODOS os dados de CADA um dos contatos presentes
+na lista chamada agenda (eventualmente chamada de agd).
+
+Implementar nas novas opções, BEM COMO nas já implementadas, todas as
+validações cabíveis.
+
+Entregar até sexta, dia 05 de maio de 2025, na forma de demonstração
+para o professor.
+'''
+def apresenteSe ():
+    print('+-------------------------------------------------------------+')
+    print('|                                                             |')
+    print('| AGENDA PESSOAL DE ANIVERSÁRIOS E FORMAS DE CONTATAR PESSOAS |')
+    print('|                                                             |')
+    print('| Prof André Luís dos Reis Gomes de Carvalho                  |')
+    print('|                                                             |')
+    print('| Versão 1.0 de 22/abril/2025                                 |')
+    print('|                                                             |')
+    print('+-------------------------------------------------------------+')
+
+def umTexto (solicitacao, mensagem, valido):
+    digitouDireito=False
+    while not digitouDireito:
+        txt=input(solicitacao)
+
+        if txt not in valido:
+            print(mensagem,'- Favor redigitar...')
+        else:
+            digitouDireito=True
+
+    return txt
+
+def opcaoEscolhida (mnu):
+    print()
+
+    opcoesValidas=[]
+    posicao=0
+    while posicao<len(mnu):
+        print (posicao+1,') ',mnu[posicao],sep='')
+        opcoesValidas.append(str(posicao+1))
+        posicao+=1
+
+    print()
+    return umTexto('Qual é a sua opção? ', 'Opção inválida', opcoesValidas)
+
+'''
+procura nom em agd e, se achou, retorna:
+uma lista contendo True e a posicao onde achou;
+MAS, se não achou, retorna:
+uma lista contendo False e a posição onde inserir,
+aquilo que foi buscado, mas nao foi encontrado,
+mantendo a ordenação da lista.
+'''
+def ondeEsta (nom,agd):
+    inicio=0
+    final =len(agd)-1
+    
+    while inicio<=final:
+        meio=(inicio+final)//2
+        
+        if nom.upper()==agd[meio][0].upper():
+            return [True,meio]
+        elif nom.upper()<agd[meio][0].upper():
+            final=meio-1
+        else: # nom.upper()>agd[meio][0].upper()
+            inicio=meio+1
+            
+    return [False,inicio]
+
+def cadastrar (agd):
+    digitouDireito=False
+    while not digitouDireito:
+        nome=input('\nNome.......: ')
+
+        resposta=ondeEsta(nome,agd)
+        achou   = resposta[0]
+        posicao = resposta[1]
+
+        if achou:
+            print ('Pessoa já existente - Favor redigitar...')
+        else:
+            digitouDireito=True
+            
+    aniversario=input('Aniversário: ')
+    endereco   =input('Endereço...: ')
+    telefone   =input('Telefone...: ')
+    celular    =input('Celular....: ')
+    email      =input('e-mail.....: ')
+    
+    contato=[nome,aniversario,endereco,telefone,celular,email]
+    
+    agd.insert(posicao,contato)
+    print('Cadastro realizado com sucesso!')
+
+def procurar (agd):
+    digitou_corretamente=False
+    while not digitou_corretamente:
+    print('Opção não implementada!')
+    # Ficar pedindo para digitar um nome até digitar um nome que existe
+    # cadastrado;
+    # mostrar então na tela TODOS os demais dados encontrados 
+    # sobre aquela pessoa.
+
+def atualizar(agd):
+    digitouDireito = False
+    while not digitouDireito:
+        nome = input("Nome da pessoa para atualizar: ")
+        
+        resposta = ondeEsta(nome, agd)
+        achou = resposta[0]
+        posicao = resposta[1]
+        
+        if not achou:
+            print("Pessoa não encontrada - Favor redigitar...")
+        else:
+            digitouDireito = True
+
+    menu_atualizacao = ["Aniversário", "Endereço", "Telefone", "Celular", "E-mail", "Finalizar atualizações"]
+    
+    while True:
+        opcao = int(opcaoEscolhida(menu_atualizacao))
+
+        if opcao == 1:
+            agd[posicao][1] = input("Novo aniversário: ")
+        elif opcao == 2:
+            agd[posicao][2] = input("Novo endereço: ")
+        elif opcao == 3:
+            agd[posicao][3] = input("Novo telefone: ")
+        elif opcao == 4:
+            agd[posicao][4] = input("Novo celular: ")
+        elif opcao == 5:
+            agd[posicao][5] = input("Novo e-mail: ")
+        else:  
+            print("Atualizações finalizadas!")
+            break
+
+    # Ficar mostrando um SUBMENU oferecendo as opções de atualizar aniversário, ou
+    # endereco, ou telefone, ou celular, ou email, ou finalizar as
+    # atualizações; ficar pedindo para digitar a opção até digitar uma
+    # opção válida; realizar a atulização solicitada; até ser escolhida a
+    # opção de finalizar as atualizações.
+    # USAR A FUNÇÃO opcaoEscolhida, JÁ IMPLEMENTADA, PARA FAZER O MENU
+
+def listar (agd):
+    print('Opção não implementada!')
+    # implementar aqui a listagem de todos os dados de todos
+    # os contatos cadastrados
+    # printar aviso de que não há contatos cadastrados se
+    # esse for o caso
+
+def excluir (agd):
+    print()
+    
+    digitouDireito=False
+    while not digitouDireito:
+        nome=input('Nome.......: ')
+        
+        resposta=ondeEsta(nome,agd)
+        achou   = resposta[0]
+        posicao = resposta[1]
+        
+        if not achou:
+            print ('Pessoa inexistente - Favor redigitar...')
+        else:
+            digitouDireito=True
+    
+    print('Aniversario:',agd[posicao][1])
+    print('Endereco...:',agd[posicao][2])
+    print('Telefone...:',agd[posicao][3])
+    print('Celular....:',agd[posicao][4])
+    print('e-mail.....:',agd[posicao][5])
+
+    resposta=umTexto('Deseja realmente excluir? ','Você deve digitar S ou N',['s','S','n','N'])
+    
+    if resposta in ['s','S']:
+        del agd[posicao]
+        print('Remoção realizada com sucesso!')
+    else:
+        print('Remoção não realizada!')
+
+# daqui para cima, definimos subprogramas (ou módulos, é a mesma coisa)
+# daqui para baixo, implementamos o programa
+# (nosso CRUD, C=create(cadastrar), R=read(recuperar),
+# U=update(atualizar), D=delete(remover,apagar)
+
+apresenteSe()
+
+agenda=[]
+
+menu=['Cadastrar Contato',\
+      'Procurar Contato',\
+      'Atualizar Contato',\
+      'Listar Contatos',\
+      'Excluir Contato',\
+      'Sair do Programa']
+
+deseja_terminar_o_programa=False
+while not deseja_terminar_o_programa:
+    opcao = int(opcaoEscolhida(menu))
+
+    if opcao==1:
+        cadastrar(agenda)
+    elif opcao==2:
+        procurar(agenda)
+    elif opcao==3:
+        atualizar(agenda)
+    elif opcao==4:
+        listar(agenda)
+    elif opcao==5:
+        excluir(agenda)
+    else: # opcao==6
+        deseja_terminar_o_programa=True
+        
+print('PROGRAMA ENCERRADO COM SUCESSO!')
